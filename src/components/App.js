7431e7f9c812a5c5a49f421206d4c7a87d415ba6
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-13 22:17:48 +0500

    Setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <h2>Pomodoro Clock</h2>
+    </div>
+  );
+}
+
+export default App;
