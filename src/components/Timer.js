eeaf29d82cadc23d0311b4e43f0713148829dc62
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-15 22:41:36 +0500

    Designing session box

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -16,11 +16,11 @@ class Timer extends Component {
         return (
             <section>
 
-                <section>
+                <section className="timer-cont">
                     <h4>{this.state.isSession?"Session":"Break"}</h4>
-                    <span>{this.props.timerMinute}</span>
-                    <span>:</span>
-                    <span>{this.state.timerSecond===0 
+                    <span className="timer-ses">{this.props.timerMinute}</span>
+                    <span className="timer-ses">:</span>
+                    <span className="timer-ses">{this.state.timerSecond===0 
                     ? "00" 
                     :this.state.timerSecond<10
                     ?"0"+this.state.timerSecond
